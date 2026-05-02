b0e3ca879f2f00994ced49e7c42e166bb5b7d12e
Tanin Ahmed <taninakond@gmail.com>
2026-05-02 10:48:36 +0600

    Fix form submission error: remove submit button name temporarily
    Use submitForm() function that removes name='submit' from button before calling
    native form.submit(), then restores the name attribute. This avoids the
    'form.submit is not a function' error caused by form elements overriding
    the submit method.

--- api-plugin/assets/js/admin.js
+++ api-plugin/assets/js/admin.js
@@ -12,7 +12,6 @@
 			var fileInput = $( '#plugin_zip' );
 			var statusDiv = $( '#myplugin-detect-status' );
 			var mediaBtn = $( '#myplugin-select-media' );
-			var nativeSubmit = versionForm[0].submit;
 
 			versionForm.on( 'submit', function( e ) {
 				var version = versionInput.val();
@@ -32,13 +31,20 @@
 				}, function( response ) {
 					if ( response.success && response.data.exists ) {
 						if ( confirm( mypluginAdmin.confirmUpdate ) ) {
-							nativeSubmit.call( versionForm[0] );
+							submitForm();
 						}
 					} else {
-						nativeSubmit.call( versionForm[0] );
+						submitForm();
 					}
 				});
 			});
+
+			function submitForm() {
+				var $submitBtn = versionForm.find( 'input[name="submit"], button[name="submit"]' );
+				$submitBtn.removeAttr( 'name' );
+				versionForm[0].submit();
+				$submitBtn.attr( 'name', 'submit' );
+			}
 
 			fileInput.on( 'change', function() {
 				var file = this.files[0];
