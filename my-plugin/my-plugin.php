26b45454e07b274dc3c156863cfe90f091dcbaa0
Tanin Ahmed <taninakond@gmail.com>
2026-05-02 16:54:23 +0600

    Update version

--- my-plugin/my-plugin.php
+++ my-plugin/my-plugin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: My Plugin
  * Description: Your awesome plugin with custom update system
- * Version: 1.0.13
+ * Version: 1.0.15
  * Author: Your Name
  * Text Domain: my-plugin
  */
@@ -12,7 +12,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('MY_PLUGIN_VERSION', '1.0.13');
+define('MY_PLUGIN_VERSION', '1.0.15');
 define('MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('MY_PLUGIN_URL', plugin_dir_url(__FILE__));
 
